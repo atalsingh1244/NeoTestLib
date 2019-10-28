d67809cc1e26f753d5bb87de03696d0b8a5341a7
atalsingh1244 <atalsinghmgst@gmail.com>
2019-10-28 17:36:57 +0530

    First commit

---
+++ NeoTestLib/NeoTestLib.h
@@ -0,0 +1,19 @@
+//
+//  NeoTestLib.h
+//  NeoTestLib
+//
+//  Created by Atal Singh on 28/10/19.
+//  Copyright © 2019 Atal Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NeoTestLib.
+FOUNDATION_EXPORT double NeoTestLibVersionNumber;
+
+//! Project version string for NeoTestLib.
+FOUNDATION_EXPORT const unsigned char NeoTestLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NeoTestLib/PublicHeader.h>
+
+
